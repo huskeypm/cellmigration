0b7f50ee68c24dbfadb99cf0c6b5a4ee24f585e8
Emily <ekrueger2@luc.edu>
2023-04-14 11:29:13 -0500

    changed run.bash file. more runs added

--- tests/run.bash
+++ tests/run.bash
@@ -5,13 +5,24 @@ export EXEC="python3 /home/ekrueger2/source/cellmigration/brown_wnonbond.py"
 #$EXEC -yamlFile "crowder_EK.yaml" -run
 #python3 brown_wnonbond.py -yamlFile "crowder_EK2.yaml" -run
 
+prefix=crowder_EK_16_crowder
+
+for i in {1..10}
+do
+	$EXEC -yamlFile ${prefix}.yaml -outName  ${prefix}_${i} -run
+
+done
+
 #Different number of crowder cases:
 #$EXEC -yamlFile crowder_EK_1_crowders.yaml -run # crowders=1
 #$EXEC -yamlFile crowder_EK_4_crowders.yaml -run # crowders=4
 #$EXEC -yamlFile crowder_EK_9_crowders.yaml -run # crowders=9
-$EXEC -yamlFile crowder_EK_16_crowders.yaml -outName crowder_EK_16_crowders_1 -run # crowders=16
-$EXEC -yamlFile crowder_EK_16_crowders.yaml -outName crowder_EK_16_crowders_2 -run # crowders=16
-$EXEC -yamlFile crowder_EK_16_crowders.yaml -outName crowder_EK_16_crowders_3 -run # crowders=16
+#$EXEC -yamlFile crowder_EK_16_crowders.yaml -outName crowder_EK_16_crowders_1 -run # crowders=16
+#$EXEC -yamlFile crowder_EK_16_crowders.yaml -outName crowder_EK_16_crowders_2 -run # crowders=16
+#$EXEC -yamlFile crowder_EK_16_crowders.yaml -outName crowder_EK_16_crowders_3 -run # crowders=16
+#$EXEC -yamlFile crowder_EK_25_crowders.yaml -outName crowder_EK_25_crowders_1 -run # crowders=16
+#$EXEC -yamlFile crowder_EK_25_crowders.yaml -outName crowder_EK_25_crowders_2 -run # crowders=16
+#$EXEC -yamlFile crowder_EK_25_crowders.yaml -outName crowder_EK_25_crowders_3 -run # crowders=16
 #$EXEC -yamlFile crowder_EK_16_crowders.yaml out crowder_EK_16_crowders_2 -run # crowders=16
 #$EXEC -yamlFile crowder_EK_16_crowders.yaml out crowder_EK_16_crowders_3 -run # crowders=16
 #$EXEC -yamlFile crowder_EK_25_crowders.yaml -run # crowders=25
